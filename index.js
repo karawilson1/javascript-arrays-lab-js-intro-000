16183d4d482f2e781ef54cf03b9ac73d553379d8
Wilson <karaewilson16@gmail.com>
2021-01-14 22:32:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"];
+kittens2 = ['Broom']
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  // kittens.push('Ralph')
+  console.log(kittens.push('Ralph'))
+}
+
+function destructivelyPrependKitten(name) {
+  //kittens.unshift('Bob')
+  console.log(kittens.unshift('Bob'))
+}
+
+function destructivelyRemoveLastKitten(name) {
+  //kittens.pop()
+  console.log(kittens.pop())
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  //kittens.shift()
+  console.log(kittens.shift())
+}
+
+function appendKitten(name) {
+  //kittens.concat('Broom')
+  return (kittens.concat(name))
+}
+
+function prependKitten(name) {
+  //kittens.slice('Arnold')
+  return (kittens.slice(name))
+}
