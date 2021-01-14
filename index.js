2b30a880e6e69b03cf2186ddcb53071d55e7e2f2
Wilson <karaewilson16@gmail.com>
2021-01-14 23:22:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten(name) {
-  //kittens.slice(0, kittens.length - 1)
+  //kittens.slice()
   console.log(kittens.slice(0, kittens.length -1))
 }
